945f5cb260a65a0716e2d072b9d1fbe801c4433b
YueYing <hero@Lenovo-PC>
2017-12-29 10:30:51 +0800

    主界面

---
+++ cvd/src/pers/zyq/frame/MainFrame.java
@@ -0,0 +1,84 @@
+package pers.zyq.frame;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Toolkit;
+
+import javax.swing.JDesktopPane;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class MainFrame extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainFrame frame = new MainFrame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public MainFrame() {
+		setTitle("\u5F71\u789F\u7BA1\u7406\u7CFB\u7EDF");
+		setIconImage(Toolkit.getDefaultToolkit().getImage(MainFrame.class.getResource("/pers/zyq/frame/vcdmana.png")));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 599, 549);
+		setExtendedState(MAXIMIZED_BOTH);
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JMenu usermenu = new JMenu("\u7528\u6237\u7BA1\u7406");
+		menuBar.add(usermenu);
+		
+		JMenuItem userAddItem = new JMenuItem("\u7528\u6237\u6CE8\u518C");
+		usermenu.add(userAddItem);
+		
+		JMenuItem userModifyItem = new JMenuItem("\u7528\u6237\u7BA1\u7406");
+		usermenu.add(userModifyItem);
+		
+		JMenu cdmenu = new JMenu("\u5F71\u789F\u7BA1\u7406");
+		menuBar.add(cdmenu);
+		
+		JMenuItem addItem = new JMenuItem("\u5F71\u789F\u5165\u5E93");
+		cdmenu.add(addItem);
+		
+		JMenuItem modifyItem = new JMenuItem("\u5F71\u789F\u7EF4\u62A4");
+		cdmenu.add(modifyItem);
+		
+		JMenu cdbrmenu = new JMenu("\u5F71\u789F\u501F\u9605\u4E0E\u5F52\u8FD8");
+		menuBar.add(cdbrmenu);
+		
+		JMenuItem cdBoItem = new JMenuItem("\u5F71\u789F\u501F\u9605");
+		cdbrmenu.add(cdBoItem);
+		
+		JMenuItem menuItem_1 = new JMenuItem("\u5F71\u789F\u5F52\u8FD8");
+		cdbrmenu.add(menuItem_1);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JDesktopPane desktopPane = new JDesktopPane();
+		desktopPane.setBackground(Color.BLUE);
+		contentPane.add(desktopPane, BorderLayout.CENTER);
+	}
+
+}
